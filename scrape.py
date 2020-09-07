d7bd806d61f6fe1c76cab0146ac598b998e2c5e0
michaelchen27 <michael.chen2701@gmail.com>
2020-09-07 16:56:58 +0700

    Hashtag (#), Tag (@), Video

--- scrape.py
+++ scrape.py
@@ -1,11 +1,9 @@
 from bs4 import BeautifulSoup
-import requests
 import selenium.webdriver as webdriver
 import openpyxl as xl
 import os
-import time
 
-edited_excel_path = "D:\\IG_Project\\poyo.xlsx"
+edited_excel_path = "D:\\IG_Project\\checked_form.xlsx"
 
 if os.path.exists(edited_excel_path):
     os.remove(edited_excel_path)
@@ -13,57 +11,65 @@ if os.path.exists(edited_excel_path):
 wb = xl.load_workbook('sorted_form.xlsx')
 sheet = wb['Form Responses 1']
 
+
 counter = 0
 for row in range(2, sheet.max_row + 1):
-    url = sheet.cell(row, 12).value
 
     driver = webdriver.Chrome(executable_path="D:\\Downloads\\chromedriver.exe")
-    driver.get(url)
 
+    url = sheet.cell(row, 12).value
+    driver.get(url)
     soup = BeautifulSoup(driver.page_source, features="lxml")
 
-    i = 0
-    tagExist = False
+    profile_url = sheet.cell(row, 10).value
+    driver.get(profile_url)
+    soup_profile = BeautifulSoup(driver.page_source, features="lxml")
+
+    username = soup_profile.find("title").text
+    username = username.split('• ')[0]
+    counter += 1
+    print(f'{counter}.{username}\n----------------------------------------')
+
 
 # Cek Tag @djikp
+    tagExist = False
+    tagCounter = 0
+
     for tag in soup.find_all('a', class_='notranslate'):
         if tag.text == "@djikp":
             tagExist = True
+        tagCounter += 1
 
-    if tagExist:
-        sheet.cell(row, 17).value = "ADA"
+    if tagExist and tagCounter >= 6:
+        sheet.cell(row, 18).value = "ADA"
+
     else:
-        sheet.cell(row, 17).value = "TIDAK ADA"
+        sheet.cell(row, 18).value = "TIDAK ADA"
 
-    wb.save("poyo.xlsx")
-# Cek Tag @djikp
+    print(f'People tagged\t: {tagCounter}')
 
 # Cek hashtag
+    i = 0
     for x in soup.find_all('a', {'class': 'xil3i'}):
         if x.text.lower() == "#ceritakemerdekaan" or x.text.lower() == "#lombapodcastkominfo":
             i += 1
 
     if i >= 2:
         sheet.cell(row, 20).value = "ADA"
-        i = 0
+        print(f'Hashtag\t\t\t: Lengkap')
 
     else:
         sheet.cell(row, 20).value = "TIDAK ADA"
-# Cek hashtag
+        print('Hashtag\t\t\t: Tidak lengkap')
 
 # Cek Durasi Video
     video = soup.find('video')
     if video:
         video_url = video['src']
-        driver.get(video_url)
-
-
+        print(f'Video URL\t\t: {video_url}')
     else:
-        print("Video doesn't exist!")
-    # sheet.cell(row, 21).value = video.duration
+        print('Video URL\t\t: null')
 
-# Cek Durasi Video
+    wb.save("checked_form.xlsx")
 
-    wb.save("poyo.xlsx")
-    counter += 1
-    print(counter)
+    print('DONE CHECKING!\n')
